bc88b7716d4156e030f7f2a7121244f50a2d9df4
Keshav Tiwari <tiwarikeshav373@gmail.com>
2026-09-05 18:10:46 +0530

    react project start

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+function Hero() {
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="/logo.png" alt="Cinevo Logo" width="90.41" height="66" />
+
+          <img src="/hero.png" alt="Hero Banner" />
+
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+      </div>
+    </main>
+  );
+}
+
+export default Hero;
